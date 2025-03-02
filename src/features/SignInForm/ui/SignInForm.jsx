9cfd476029ef34fb501169a3756f24268f431039
Anzhelika <anzhelikasilkina@yandex.ru>
2025-03-02 09:13:53 +0300

    fix styles, improve validation

--- src/features/SignInForm/ui/SignInForm.jsx
+++ src/features/SignInForm/ui/SignInForm.jsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, Typography, Button } from "@mui/material";
 import { Link } from "react-router";
-
-import styles from "./SignInForm.module.scss";
+import { useForm } from "react-hook-form";
+import styles from "shared/ui/Form/Form.module.scss";
 
 const SignInForm = () => {
   const sxStyles = {
@@ -11,16 +11,64 @@ const SignInForm = () => {
     button: { backgroundColor: "#1890FF" },
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Card sx={sxStyles.card}>
       <CardContent>
         <h2 className={styles.header}>Sign In</h2>
-        <form action="" className={styles.form}>
-          <label>Email address</label>
-          <input type="text" required className={styles.input} />
-          <label>Password</label>
-          <input type="text" required className={styles.input} />
-          <Button variant="contained" sx={sxStyles.button}>
+        <form action="" className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email">
+            {" "}
+            Email
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Email"
+              id="email"
+              {...register("email", {
+                required: "Required field",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+              })}
+            />
+            <div className={errors?.email && styles.inputError}>
+              {errors?.email && <p>{errors?.email?.message || "You should use a valid email"}</p>}
+            </div>
+          </label>
+
+          <label htmlFor="password">
+            {" "}
+            Password
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Password"
+              id="password"
+              {...register("password", {
+                required: "Required field",
+                minLength: {
+                  value: 6,
+                  message: "Password should be at least 6 characters",
+                },
+                maxLength: { value: 40, message: "Password should not be longer than 40 characters" },
+              })}
+            />
+            <div className={errors?.password && styles.inputError}>
+              {errors?.password && <p>{errors?.password?.message || "Password should be 6 to 40 characters"}</p>}
+            </div>
+          </label>
+          <Button variant="contained" type="submit" sx={sxStyles.button}>
             Login
           </Button>
           <Typography sx={sxStyles.formSignIn}>
--- src/features/SignUpForm/ui/SignUpForm.jsx
+++ src/features/SignUpForm/ui/SignUpForm.jsx
@@ -1,35 +1,116 @@
-import { Card, CardContent, Typography, Checkbox, Button, FormControlLabel } from "@mui/material";
+import { Card, CardContent, Typography, Checkbox, Button } from "@mui/material";
 import { Link } from "react-router";
-
-import styles from "./SignUpForm.module.scss";
+import { useForm } from "react-hook-form";
+import styles from "shared/ui/Form/Form.module.scss";
 
 const SignUpForm = () => {
   const sxStyles = {
     card: { display: "flex", flexDirection: "column", marginTop: 2, width: 380, padding: 2 },
     form: { display: "flex", flexDirection: "column", gap: 1 },
-    formSignIn: { alignSelf: "center" },
+    formSignIn: { alignSelf: "center", fontSize: 14 },
     button: { backgroundColor: "#1890FF" },
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <Card sx={sxStyles.card}>
       <CardContent>
         <h2 className={styles.header}>Create new account</h2>
-        <form action="" className={styles.form}>
-          <label>Username</label>
-          <input type="text" required className={styles.input} />
-          <label>Email address</label>
-          <input type="text" required className={styles.input} />
-          <label>Password</label>
-          <input type="text" required className={styles.input} />
-          <label>Repeat Password</label>
-          <input type="text" required className={styles.input} />
-          <FormControlLabel
-            label="I agree to the processing of my personal information"
-            required
-            control={<Checkbox />}
-          />
-          <Button variant="contained" sx={sxStyles.button}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <label htmlFor="username">
+            {" "}
+            Username
+            <input
+              id="username"
+              className={styles.input}
+              type="text"
+              placeholder="Username"
+              {...register("userName", { required: "Required field", max: 20, min: 3, maxLength: 30 })}
+            />
+            <div className={errors?.userName && styles.inputError}>
+              {errors?.userName && <p>{errors?.userName?.message || "Username should be 3 to 20 characters"}</p>}
+            </div>
+          </label>
+
+          <label htmlFor="email">
+            {" "}
+            Email
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Email"
+              id="email"
+              {...register("email", {
+                required: "Required field",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+              })}
+            />
+            <div className={errors?.email && styles.inputError}>
+              {errors?.email && <p>{errors?.email?.message || "You should use a valid email"}</p>}
+            </div>
+          </label>
+
+          <label htmlFor="password">
+            {" "}
+            Password
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Password"
+              id="password"
+              {...register("password", {
+                required: "Required field",
+                minLength: {
+                  value: 6,
+                  message: "Password should be at least 6 characters",
+                },
+                maxLength: { value: 40, message: "Password should not be longer than 40 characters" },
+              })}
+            />
+            <div className={errors?.password && styles.inputError}>
+              {errors?.password && <p>{errors?.password?.message || "Password should be 6 to 40 characters"}</p>}
+            </div>
+          </label>
+
+          <label htmlFor="repeatPassword">
+            {" "}
+            Repeat password
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Repeat password"
+              id="repeatPassword"
+              {...register("repeatPassword", {
+                required: "Required field",
+                validate: {
+                  checkPassword: (repeatPassword, { password }) => {
+                    return repeatPassword !== password ? "Passwords don't match" : null;
+                  },
+                },
+              })}
+            />
+            <div className={errors?.repeatPassword && styles.inputError}>
+              {errors?.repeatPassword && <p>{errors?.repeatPassword?.message || "Error!"}</p>}
+            </div>
+          </label>
+          <div className={styles.checkbox}>
+            <Checkbox required />
+            <label className={styles.checkboxLabel}>I agree to the processing of my personal information</label>
+          </div>
+
+          <Button variant="contained" type="submit" sx={sxStyles.button}>
             Create
           </Button>
           <Typography sx={sxStyles.formSignIn}>
